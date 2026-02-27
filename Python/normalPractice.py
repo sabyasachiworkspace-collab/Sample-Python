70fb5f951bea4564a33dd26ea4d761f6f769f08f
sabyasachiworkspace-collab <sabyasachiworkspace@gmail.com>
2026-02-27 19:55:31 +0530

    Add files via upload

---
+++ Python/normalPractice.py
@@ -0,0 +1,33 @@
+# code for add two numbers
+# print("Enter two number");
+# a= int(input("Enter first no."));
+# b = int(input("enter second number. "));
+# c = a+b;
+# print( "Sum of the number is: ",c);
+
+#finding  remainder..
+# print ("Enter two numbers");
+# a= int(input("Enter first no."));
+# z = int (input("Enter second number."));
+# b = a%z;
+# print("Here is the remaider of the number.", b);
+
+# comparaison 
+# a = 34;
+# b=80;
+# print(a>b);
+
+# finding average
+# print("enter two number to find the average");
+# a = int (input("num1"));
+# b = int (input("num2"));
+# c = (a+b)/2;
+# print("The average is of ",a,"and ", b, " are ", c );
+
+#finding of square of two numbers;
+# print("Enter a number for square root");
+# a = int(input('ENnter a number '));
+# squr = a*a ;
+# print("the square of the number is:", squr);
+
+ 
---
+++ Python/striingPractice.py
@@ -0,0 +1,20 @@
+# greetings using f strings
+'''name = input("Enter your name ")
+print(f"Good Afternoon {name}")'''
+
+#fill name and date;;
+""" letter = '''Dear <|NAME|> 
+you are selected! 
+<|Date|>'''
+
+print(letter.replace("<|NAME|>", "Sabyasachi").replace("<|Date|>", "6 Nov 2026"))"""
+
+# detect double space 
+'''name  = "Sabyasachi is a  good boy"
+print(name.find("  ")) #finding double space
+print(name.replace("  "," ")) # replace double sapace by single space.
+# STRINGS ARE IMMUATABLE WHICH MEANS THAT YOU CANNOT CHANGE THEM BY RUNNIG FUNCTIONS ON THEM'''
+
+
+
+ 
